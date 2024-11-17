0e7133adb446c2203f80616e497b5ad3c02c5e69
조수현 <josuhyeon@josuhyeon-ui-MacBookAir.local>
2024-11-17 13:40:07 +0900

    스와이프 기능 수정 및 안내문구 작성

--- src/pages/Mainpage/Mainpage.js
+++ src/pages/Mainpage/Mainpage.js
@@ -12,16 +12,15 @@ function Mainpage() {
             {/* 사이트 소개 */}
             <div className="start">
                 <p>From. 이화여대CCC</p>
-                <p>25학번 예비 새내기를 위한 응원편지가 도착했어요!</p>
-            </div>
-            {/* 편지 사진 슬라이드 */}
-            <LetterSlider />
+                <p>25학번 예비 새내기를 위한<br />응원편지가 도착했어요!</p>
 
-               {/* 버튼 */}
-               <button className="more_letter">
-               <a href="/human" style={{ textDecoration: 'none', color: 'inherit' }}><p>단과대별 선배들 편지 더보기</p></a>
-        
-            </button>
+                <div className="guide">
+                    <p>&lt;--  단과대별 선배들 편지 더보기 (좌우 쓸어넘기기)  --&gt;</p>
+                </div>
+                {/* 편지 사진 슬라이드 */}
+                <LetterSlider />
+
+            </div>
         </div >
     );
 }
